34caca4b04fcfac4a346137ab36a1e523ddff252
ric2k1 <cyhuang@ntu.edu.tw>
2022-12-15 19:56:12 +0800

    Upgrade to graphql 16.6.0

--- backend/src/resolvers/Subscription.js
+++ backend/src/resolvers/Subscription.js
@@ -9,12 +9,12 @@ const Subscription = {
         throw new Error('Post not found');
       }
 
-      return pubsub.asyncIterator(`comment ${postId}`);
+      return pubsub.subscribe(`comment ${postId}`);
     },
   },
   post: {
     subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator('post');
+      return pubsub.subscribe('post');
     },
   },
 };
